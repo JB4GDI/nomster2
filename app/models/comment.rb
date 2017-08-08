33ca0a6f5c755f7283c06fd2d523e9ad262d72e2
Jaime Barriga <JB4GDI@gmail.com>
2017-08-08 02:56:35 +0000

    Updated the mailer to trigger on comment creation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+  # right after we create a new one, call this function
+  after_create :send_comment_email
 
   # User sees what's on the left side.  Right is stored in DB.
   RATINGS = {
@@ -13,6 +15,11 @@ class Comment < ApplicationRecord
 
   def humanized_rating
     RATINGS.invert[self.rating]
+  end
+
+  def send_comment_email
+    # Notice that this was basically what we were doing in the Rails command line
+    NotificationMailer.comment_added(self).deliver
   end
 
 end
